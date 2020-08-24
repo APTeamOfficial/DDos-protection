630f124d00e9027fd6d656fe3736a4d1fd04d0dc
Ali omidi <ph09nixom@gmail.com>
2020-08-24 10:52:55 +0430

    version 1

---
+++ db_example.sql
@@ -0,0 +1,37 @@
+-- phpMyAdmin SQL Dump
+-- version 4.6.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Aug 24, 2020 at 06:22 AM
+-- Server version: 5.7.14
+-- PHP Version: 5.6.25
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `adp`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tokens_pool`
+--
+
+CREATE TABLE `tokens_pool` (
+  `token` varchar(40) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  `ip` varchar(50) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  `time` varchar(100) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
